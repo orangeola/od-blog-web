d0627341243262001697c55b55c3ba4bc6633e15
orangeola <orangeqmm@gmail.com>
2023-03-16 01:53:34 +1030

    Submit comments

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,19 +1,50 @@
 import '../styles/CommentForm.css'
+import { useParams, } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
 
 function CommentForm() {
+  const {id} = useParams();
+  let [author, setAuthor] = useState(null);
+  let [text, setText] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+    let res = await fetch(`http://localhost:5000/post/${id}/comment`, {
+       method: 'POST',
+       body: JSON.stringify({
+          author: author,
+          text: text,
+       }),
+       headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+       },
+    })
+    let resJson = await res.json();
+    if (res.status === 200) {
+      console.log("Message created successfully");
+    } else {
+      console.log("Some error occured", resJson.errors);
+    }
+  }
+  catch(err){
+    console.log("err: ", err);
+  }
+  };
+
   return (
     <div className="CommentForm">
-      <form>
+      <form onSubmit={handleSubmit}>
         <p><i>Submit a comment!</i></p>
         <label>
           Name:
           <br></br>
-          <input type="text" name="author" />
+          <input type="text" name="author" onChange={(e) => setAuthor(e.target.value)}/>
         </label>
         <label>
           Comment:
           <br></br>
-          <textarea type="text" name="text" />
+          <textarea type="text" name="text" onChange={(e) => setText(e.target.value)}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
