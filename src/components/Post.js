2fcab1e90b715adc19bdbe44f736c352c25bab69
orangeola <orangeqmm@gmail.com>
2023-03-15 22:08:11 +1030

    Call proper API when in the dynamic URL of a post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useParams, } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
 
 function Post() {
   const {id} = useParams();
+  let [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/post/${id}`)
+   .then(response => response.json())
+   .then(data => {
+    console.log(data.post[0])
+    setPost(data.post[0]);
+    })
+  }, []);
+
   return (
     <div className="Post">
-      <h1>Post: {id}</h1>
+      {post && 
+      <div>
+        <h1>{post.title}</h1>
+        <p>{post.text}</p>
+        <p>Posted at: {post.date}</p>
+      </div>
+      }
     </div>
   );
 }
