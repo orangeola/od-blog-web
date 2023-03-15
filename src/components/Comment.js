f011b00cd1a6c5c5611e99b48597e8678b4dfe83
orangeola <orangeqmm@gmail.com>
2023-03-15 22:24:25 +1030

    Display comments when on a posts URL

---
+++ src/components/Comment.js
@@ -0,0 +1,33 @@
+import { useParams, } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+
+function Comment(props) {
+  const {id} = useParams();
+  let [comments, setComments] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/post/${id}/comment`)
+   .then(response => response.json())
+   .then(data => {
+    console.log(data.comment)
+    setComments(data.comment);
+    })
+  }, []);
+
+  return (
+    <div className="Comment">
+      <div>
+        {comments && comments.map((task) => {
+          return <div key={task._id}>
+            <i>{task.author}</i><br></br>
+            <i>{task.date}</i><br></br>
+            {task.text}<br></br>
+            <hr></hr>
+            </div>;
+        })}
+    </div>
+    </div>
+  );
+}
+
+export default Comment;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import { useParams, } from "react-router-dom";
 import React, {useState, useEffect} from 'react';
+import Comment from "./Comment"
 
 function Post() {
   const {id} = useParams();
@@ -21,6 +22,9 @@ function Post() {
         <h1>{post.title}</h1>
         <p>{post.text}</p>
         <p>Posted at: {post.date}</p>
+        <h3>Comments</h3>
+        <hr></hr>
+        <Comment />
       </div>
       }
     </div>
